733373cf14e4109d2f41ff75380779f6823b6870
Christopher Kollars <599651+ckollars@users.noreply.github.com>
2021-07-23 10:55:02 -0500

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,44 +1,25 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
-
-# GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-brew install moreutils
-# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-brew install findutils
-
-
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-
-# Install `wget` with IRI support.
+brew install ack
+brew install diff-so-fancy
+brew install exa
+brew install fd
 brew install wget --with-iri
-
-# Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
-
 brew install git
 brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
-
-brew install terminal-notifier
-
+brew install node
 brew install zsh
+brew install nvm
+
+# Apps
+brew install --cask 1password
+brew install --cask meetingbar
+brew install --cask microsoft-auto-update
+brew install --cask sketch
+brew install --cask alfred
+brew install --cask visual-studio-code
+brew install --cask rectangle
+brew install --cask bartender
 
 # Remove outdated versions from the cellar.
 brew cleanup
